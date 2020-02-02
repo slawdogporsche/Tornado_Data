3223059280be4c1ec27254f8ed12ed00236ebd02
slawdogporsche <benihana320@gmail.com>
2020-02-01 22:35:20 -0500

    update to script

--- Tornado_Map.R
+++ Tornado_Map.R
@@ -1,26 +1,32 @@
 ## Tornado Visualization ## 
 
 ## Setup ## 
-
 library(sf)
 library(tidyverse)
+library(maps)
 
 setwd("C:/Users/Ben/Desktop/Tornado_Data/1950-2018-torn-aspath")
 
 ## Data ##
-
 t_data <- st_read("1950-2018-torn-aspath.shp")
 
+maps <- map_data("world") %>%
+  as_tibble() %>%
+  filter(region == "USA")
+
 ## Validation ## 
-
 st_crs(t_data)
-
 st_bbox(t_data)
 
 ## Visualization ## 
-
-t_data %>%
+t_data[1:50,] %>%
   ggplot() +
-  geom_polygon(aes(x=slat, y= slon, color = "red")) +
-  coord_fixed(1.3)
+   geom_point(aes(x=slon, y= slat, color = "blue")) +
+  coord_fixed(1.3) +
+  geom_polygon(data = maps, aes(x = long, y=lat, group = group,  fill = region)) +
+  
+  
+  
+  
+  
 
