9d9bcde7aec1d418823c5ad385ef855b8e7439b4
slawdogporsche <benihana320@gmail.com>
2020-02-01 22:50:28 -0500

    final notes

--- Tornado_Map.R
+++ Tornado_Map.R
@@ -1,13 +1,15 @@
 ## Tornado Visualization ## 
 
 ## Setup ## 
+
 library(sf)
 library(tidyverse)
 library(maps)
 
-setwd("C:/Users/Ben/Desktop/Tornado_Data/1950-2018-torn-aspath")
+  setwd("C:/Users/Ben/Desktop/Tornado_Data/1950-2018-torn-aspath")
 
 ## Data ##
+
 t_data <- st_read("1950-2018-torn-aspath.shp")
 
 maps <- map_data("world") %>%
@@ -15,10 +17,13 @@ maps <- map_data("world") %>%
   filter(region == "USA")
 
 ## Validation ## 
+
 st_crs(t_data)
+
 st_bbox(t_data)
 
 ## Visualization ## 
+## Have to somehow use geometry field to show change over time 
 t_data %>%
   ggplot() +
   geom_polygon(data = maps, aes(x = long, y=lat, group = group)) +
